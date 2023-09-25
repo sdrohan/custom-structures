d4b402a205a728c6f671523fb5639be33d676209
Siobhan Drohan <siobhan.drohan@setu.ie>
2023-09-25 17:31:30 +0100

    Add Bespoke Stack Implementation

--- src/main/kotlin/ui/StackScreen.kt
+++ src/main/kotlin/ui/StackScreen.kt
@@ -1,2 +1,20 @@
 package ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StackScreen() {
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                "Stack",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+}
