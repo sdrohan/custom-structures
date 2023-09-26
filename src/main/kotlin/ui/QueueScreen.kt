17149ccc2b18eee2aedd6027c796d53b72ea575d
Siobhan Drohan <siobhan.drohan@setu.ie>
2023-09-26 15:40:50 +0100

    Add QueueScreen with enque and display

---
+++ src/main/kotlin/ui/QueueScreen.kt
@@ -0,0 +1,62 @@
+package ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import datastructures.CustomQueue
+import interfaces.Queue
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QueueScreen() {
+
+    var inputText by remember { mutableStateOf("") }
+    val queue by remember { mutableStateOf(CustomQueue<String>()) }
+
+    Column(Modifier.padding(16.dp))
+    {
+        Text(
+            "Queue",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        OutlinedTextField(
+            value = inputText,
+            onValueChange = { inputText = it },
+            label = { Text("Enter element") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(onClick = {
+            enqueueElement(queue, inputText)
+            inputText = ""
+        }) {
+            Text("Enqueue")
+        }
+
+        Text("Queue Size: ${queue.size()}")
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("Queue Elements:")
+        Spacer(modifier = Modifier.height(8.dp))
+
+        for(i in 0..queue.size()){
+            queue.peek(i)?.let { Text(it) }
+        }
+
+    }
+}
+
+private fun enqueueElement(queue: Queue<String>, element: String) {
+    if (element.isNotBlank()) {
+        queue.enqueue(element)
+        println(element)
+    }
+}
