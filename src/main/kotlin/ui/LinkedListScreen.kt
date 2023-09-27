2eea6d82b5da7fbe409745c628eb8879a4c1e0e2
Siobhan Drohan <siobhan.drohan@setu.ie>
2023-09-27 11:36:28 +0100

    AddFirst and AddLast in LinkedListScreen UI

--- src/main/kotlin/ui/LinkedListScreen.kt
+++ src/main/kotlin/ui/LinkedListScreen.kt
@@ -1,23 +1,22 @@
 package ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import interfaces.LinkedList
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LinkedListScreen(list: LinkedList<String>) {
 
-    val listSize by remember { mutableStateOf(list.size()) }
+    var listSize by remember { mutableStateOf(list.size()) }
+    var inputText by remember { mutableStateOf("") }
 
     Column(Modifier.padding(16.dp))
     {
@@ -29,9 +28,38 @@ fun LinkedListScreen(list: LinkedList<String>) {
         )
 
         Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = inputText,
+            onValueChange = { inputText = it },
+            label = { Text("Enter element") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(onClick = {
+            list.addFirst(inputText)
+            listSize = list.size()
+            inputText = ""
+        }) {
+            Text("Add First")
+        }
+
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(onClick = {
+            list.addLast(inputText)
+            listSize = list.size()
+            inputText = ""
+        }) {
+            Text("Add Last")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
         Text("List Size: $listSize")
 
         Spacer(modifier = Modifier.height(8.dp))
         Text("List Elements:")
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(list.toString())
     }
 }
